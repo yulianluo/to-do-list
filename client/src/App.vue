34162001bce80d8b2aec50e9d5a7bba7664cbcdb
yulian <yulianlluo@gmail.com>
2020-05-22 00:27:36 +0800

    app bar added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,26 @@
 <template>
   <v-app>
+    <v-app-bar fixed color="indigo" dense dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+
     <div id="app">
       <div id="nav">
         <router-link to="/">Home</router-link>|
         <router-link to="/about">About</router-link>
+        <p>asdasdasda</p>
       </div>
       <router-view />
     </div>
--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,17 @@
 import { sync } from 'vuex-router-sync';
-import Vuetify from 'vuetify';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
-Vue.use(Vuetify);
+
 sync(store, router);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
