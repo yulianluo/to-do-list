118bd38707ce6bfe0ad199339fc789041cb39610
yulian <yulianlluo@gmail.com>
2020-05-21 20:21:05 +0800

    index create task

--- server/start/routes.js
+++ server/start/routes.js
@@ -31,5 +31,7 @@ Route.group(() => {
 	Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth'); 
 	Route.patch('projects/:id', 'ProjectController.update').middleware('auth'); 
 
+	Route.get('projects/:id/tasks', 'TaskController.index').middleware('auth'); 
+	Route.post('projects/:id/tasks', 'TaskController.create').middleware('auth'); 
 })
 	.prefix('api');
